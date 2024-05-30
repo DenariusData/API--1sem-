b9f8d5724d6f5cba4c455dcb156d2f370e7e1790
Thiago <teagos371@gmail.com>
2024-05-30 18:04:23 -0300

    Corrigido tipagem das variaveis

--- src/operacoes/bhaskara.ts
+++ src/operacoes/bhaskara.ts
@@ -1,34 +1,34 @@
-import promptSync from 'prompt-sync';
 import * as readlineSync from 'readline-sync';
 
-const prompt = promptSync();
-
 function eq2grau(a: number, b: number, c: number): { x1: number, x2: number, delta: number } | string {
-    const delta = b * b - 4 * a * c;
+    const delta: number = b * b - 4 * a * c;
     if (delta < 0) 
         {
             console.log("O valor de delta é: " + delta);
             return "Não existem raízes reais.";
         }
 
-    let sqrtDelta = delta;
-    let previous;
+    let sqrtDelta: number = delta;
+    let previous: number;
 
     do {
         previous = sqrtDelta;
         sqrtDelta = (previous + delta / previous) / 2;
     } while (sqrtDelta !== previous);
 
-    const x1 = (-b + sqrtDelta) / (2 * a);
-    const x2 = (-b - sqrtDelta) / (2 * a);
+    const x1: number = (-b + sqrtDelta) / (2 * a);
+    const x2: number = (-b - sqrtDelta) / (2 * a);
 
     return { x1, x2, delta };
 }
 
 export function bhaskara() {
-    const a = +readlineSync.question('Digite o valor de a: ');
-    const b = +readlineSync.question('Digite o valor de b: ');
-    const c = +readlineSync.question('Digite o valor de c: ');
+    console.log("EQUACAO DE SEGUNDO GRAU");
+    console.log();
+
+    const a: number = +readlineSync.question('Digite o valor de a: ');
+    const b: number = +readlineSync.question('Digite o valor de b: ');
+    const c: number = +readlineSync.question('Digite o valor de c: ');
 
     const resultado = eq2grau(a, b, c);
     if (typeof resultado === 'string') {
